1d654a20503cc819d7e93106b8146597e3ddbd52
BAKYTq1 <bsainaev@gmail.com>
2025-07-09 23:56:24 +0600

    'nelikvid'

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles/App.scss'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+function App() {
+  return (
+    <div className='container'>
+      <Header/>
+      
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
