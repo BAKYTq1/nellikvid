60ebc013c685287d5fd750c93581f53fdcb8d767
mirbek10 <mirbekbook@gmail.com>
2025-07-11 13:41:33 +0600

    save

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './styles/App.scss'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 function App() {
-  return (
-    <div className='container'>
+  return (<>
       <Header/>
+  
+    <div className='container'>
       
-      <Footer/>
     </div>
+      <Footer/>
+  </>
   )
 }
 
