74d7ac154f8d6755166c9095999a6764940838ee
BAKYTq1 <bsainaev@gmail.com>
2025-07-12 15:13:05 +0600

    baha/dev

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Bestseller from './components/Bestseller/Bestseller'
 import Manufacturers from './components/Manufacturers/Manufacturers'
+import { Order } from './components/Order/Order'
 function App() {
   return (
-    <div className='container'>
+    <div>
       <Header/>
       <Bestseller/>
       <Manufacturers/>
+      <Order/>
       <Footer/>
     </div>
   )
