397d4a3e9bcd3986d11145e52f8eca3850a5ad57
Nurzada <nurzadatobokelova@gmail.com>
2025-07-12 14:59:29 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import  'react'
-import './styles/App.scss'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import Bestseller from './components/Bestseller/Bestseller'
-import Manufacturers from './components/Manufacturers/Manufacturers'
-import Price from './components/Price/Price'
-import Brand from './components/Brand/Brand'
+import "react";
+import "./styles/App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Bestseller from "./components/Bestseller/Bestseller";
+import Manufacturers from "./components/Manufacturers/Manufacturers";
+import Price from "./components/Price/Price";
+import Brand from "./components/Brand/Brand";
 function App() {
   return (
-    <div className=''>
-      <Header/>
-      <Bestseller/>
-      <Manufacturers/>
-      <Price/>
-<Brand/>
+    <div className="">
+      <Header />
+      <Bestseller />
+      <Manufacturers />
+      <Price />
+      <Brand />
 
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
