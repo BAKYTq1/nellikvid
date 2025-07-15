b589a389c404bcbe40c3f4d8d560cdc63f78e3d5
mirbek10 <mirbekbook@gmail.com>
2025-07-15 23:30:14 +0600

    review

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Bestseller from "./components/Bestseller/Bestseller";
 import Manufacturers from "./components/Manufacturers/Manufacturers";
 import Price from "./components/Price/Price";
 import Brand from "./components/Brand/Brand";
+import WorkSteps from "./components/WorkSteps/WorkSteps";
+import Review from "./components/Review/Review";
 function App() {
   return (
     <div className="">
@@ -14,7 +16,8 @@ function App() {
       <Manufacturers />
       <Price />
       <Brand />
-
+      <WorkSteps />
+      <Review />
       <Footer />
     </div>
   );
